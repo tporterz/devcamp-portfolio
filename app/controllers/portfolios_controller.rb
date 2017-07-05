94598fb79f21fe8c9c02b08e8195a1a2168abe96
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 15:19:46 +0000

    Added session tracker for dynamic source links

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,19 +1,23 @@
 class PortfoliosController < ApplicationController
   def index
-      @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.all
   end
-  
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
   def new
-      @portfolio_item = Portfolio.new
-      3.times { @portfolio_item.technologies.build }
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
   end
-  
+
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
@@ -26,6 +30,7 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -34,11 +39,11 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
+
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
-  
+
   def destroy
     # Perform the lookup
     @portfolio_item = Portfolio.find(params[:id])
@@ -51,9 +56,5 @@ class PortfoliosController < ApplicationController
       format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
-  
-  def angular
-    @angular_portfolio_items = Portfolio.angular
-  end
-  
+
 end

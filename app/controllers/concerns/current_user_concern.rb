123366c0c8e74930b89938326602582b125df3f6
Jordan Hudgens <jordan@devcamp.com>
2017-07-10 14:45:31 +0000

    Made first_name Guest and last_name User have their first letters capitalized

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -7,8 +7,8 @@ module CurrentUserConcern
   
   def guest_user
     OpenStruct.new(name: "Guest User",
-                  first_name: "guest",
-                  last_name: "user",
+                  first_name: "Guest",
+                  last_name: "User",
                   email: "guest@example.com"
                   )
   end

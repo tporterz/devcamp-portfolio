b52c1f66db5aa0617f6a9b2607eb58b00210665c
Jordan Hudgens <jordan@devcamp.com>
2017-07-13 15:10:39 +0000

    Installed styles for application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,8 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
-
+  
   def copyright_generator
-    DevcampViewTool::Renderer.copyright 'Jordan Hudgens', 'All rights reserved'
+    TylerViewTool::Renderer.copyright 'Tyler Porter', 'All rights reserved'
   end
 end

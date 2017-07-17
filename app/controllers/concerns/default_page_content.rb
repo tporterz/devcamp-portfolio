5661b659c5e1a971e3f5633a17c041cd24b8bc7f
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 15:04:01 +0000

    Updated styles and added map for contact page

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
-    @seo_keywords = "Jordan Hudgens portfolio"
+    @page_title = "Carousel Hat"
+    @seo_keywords = "Tyler Porter portfolio"
   end
 end
